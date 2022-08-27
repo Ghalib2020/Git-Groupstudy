a7cb342dee11362ea3471e7c02f8f4f8709437b8
GroupstudyPm <gasadullah1@gmail.com>
2022-08-26 20:10:43 -0500

    adding git to rep

---
+++ src/main/java/Com/nexttech/Git_Groupstudy/App.java
@@ -0,0 +1,13 @@
+package Com.nexttech.Git_Groupstudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
